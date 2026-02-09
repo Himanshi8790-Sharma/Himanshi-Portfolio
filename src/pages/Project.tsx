e4c93328032ec038d83908ca57360774f1aed59c
Himamshi <004himanshi@gmail.com>
2026-02-09 15:17:55 +0530

    remove unused react import

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import  { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { HiSparkles, HiCode, HiExternalLink, HiEye } from "react-icons/hi";
 import { FaGithub } from "react-icons/fa";
